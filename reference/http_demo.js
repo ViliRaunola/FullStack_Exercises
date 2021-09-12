0323449cb2f3e557317af5cfb1c78e28777038ce
ViliRaunola <61086026+ViliRaunola@users.noreply.github.com>
2021-09-12 17:08:59 +0300

    Delete reference directory

--- reference/http_demo.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-//Luodaan serveri objecti
-http.createServer((req, res) => {
-    //Kirjoita vastaus
-    res.write('Hello World!');
-    res.end();
-}).listen(5000, () => console.log('Server running...'))
